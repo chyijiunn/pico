d88f2cb60b3908d803eb7f911e63913b1a50ec8f
chyijiunn <chyijiunn@gmail.com>
2022-05-18 13:06:53 +0800

    Update 11_motor_sg90.py

--- 11_motor_sg90.py
+++ 11_motor_sg90.py
@@ -2,5 +2,5 @@ from machine import Pin ,PWM
 from utime import sleep
 servoPIN = PWM(Pin(16))
 servoPIN.freq(50)
-servoPIN.duty_u16(x)    # x = 1000 ~ 9000
+servoPIN.duty_u16(5000)    # x = 1000 ~ 9000
 sleep(1)
